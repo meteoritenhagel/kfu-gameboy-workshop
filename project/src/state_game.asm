f283d7d0a8fa5f54ad3828191dcf090e7d4f0ff6
Tristan Repolusk <tristan@repolusk.eu>
2026-04-07 09:47:56 +0200

    06. Set up the graphics for the gameplay state
    *) Moved game.png from raw_resources into the project folder
    *) Added a StopSound function to utils.asm for muting all sounds/music
    *) Added the game screen tiles and tilemap to state_game.asm
    *) Copy the tiles and tilemap into the VRAM

--- project/src/state_game.asm
+++ project/src/state_game.asm
@@ -6,6 +6,44 @@ SECTION "State Game Functions", ROM0
 
 ; InitStateTitle starts the game state
 InitStateGame::
+    ; Stop all sounds/music
+    call StopSounds
+
+    call WaitVBlank
+
+    ; Turn the LCD off
+    xor a
+    ld [rLCDC], a
+
+	; Copy Title Tiles
+	ld de, GameTiles
+    ld hl, $9000
+    ld bc, GameTilesEnd - GameTiles
+	call Memcopy
+
+	; Copy Title Tilemap
+	ld de, GameTilemap
+    ld hl, $9800
+    ld bc, GameTilemapEnd - GameTilemap
+	call Memcopy
+
+    ; Turn the LCD on
+    ld a, LCDC_ON | LCDC_BG_ON
+    ld [rLCDC], a
     
 GameplayLoop:
     jr GameplayLoop
+
+
+
+
+SECTION "Game Tiles", ROM0
+GameTiles:
+    INCBIN "./build/game.2bpp"
+GameTilesEnd:
+
+
+SECTION "Game Tilemaps", ROM0
+GameTilemap:
+    INCBIN "./build/game.tilemap"
+GameTilemapEnd:
