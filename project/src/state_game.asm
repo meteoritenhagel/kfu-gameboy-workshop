93593e7a4f4aae06ebb65d97d3ed425ffbdb88bd
Tristan Repolusk <tristan.repolusk@uni-graz.at>
2026-05-23 22:21:25 +0200

    07. Set up everything to draw text on screen
    *) Move font graphics and charmap from raw_resources into the project directory
    *) Define the text in memory with end of line character
    *) Import the font tiles and load them into VRAM
    *) Write a function DrawText in utils.asm for displaying text on screen
    *) Define the positions where the text should be drawn: TOP_TEXT_REGION_START and BOTTOM_TEXT_REGION_START
    *) Combine all elements to draw the text on screen in state_game.asm

--- project/src/state_game.asm
+++ project/src/state_game.asm
@@ -1,5 +1,9 @@
+INCLUDE "./include/charmap.inc"  ; specify the encoding that is used for the font characters
 INCLUDE "./include/constants.inc"
 INCLUDE "./include/hardware.inc"
+
+DEF TOP_TEXT_REGION_START EQU $9862
+DEF BOTTOM_TEXT_REGION_START EQU $9962
 
 
 SECTION "State Game Functions", ROM0
@@ -27,6 +31,20 @@ InitStateGame::
     ld bc, GameTilemapEnd - GameTilemap
 	call Memcopy
 
+    ; Copy Font Tiles into tile memory $8800
+	ld de, FontTiles
+    ld hl, $8800
+    ld bc, FontTilesEnd - FontTiles
+	call Memcopy
+
+    ; Draw the text
+    ld de, TOP_TEXT_REGION_START
+    ld hl, TopText
+    call DrawText
+    ld de, BOTTOM_TEXT_REGION_START
+    ld hl, BottomText
+    call DrawText
+
     ; Turn the LCD on
     ld a, LCDC_ON | LCDC_BG_ON
     ld [rLCDC], a
@@ -35,12 +53,19 @@ GameplayLoop:
     jr GameplayLoop
 
 
+SECTION "Game Text", ROM0
+TopText: db "COPY THE RHYTHM!", 255  ; 255 signifies end of line
+BottomText: db "YOU", 255
 
 
 SECTION "Game Tiles", ROM0
 GameTiles:
     INCBIN "./build/game.2bpp"
 GameTilesEnd:
+
+FontTiles:
+    INCBIN "./build/font.2bpp"
+FontTilesEnd:
 
 
 SECTION "Game Tilemaps", ROM0
